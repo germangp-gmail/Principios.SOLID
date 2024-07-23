bbd280b77cc45e1255d607169a562d59a5cda2b4
germangp <german.gutierrez@semicrol.com>
2024-07-23 13:18:26 +0200

    Proyecto sin cumplir SOLID

---
+++ Empleado.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public decimal SalarioBase { get; set; }
+
+        public Empleado(string nombre, string puesto, decimal salarioBase)
+        {
+            Nombre = nombre;
+            Puesto = puesto;
+            SalarioBase = salarioBase;
+        }
+
+        public void CalcularSalario()
+        {
+            if (Puesto == "Gerente")
+            {
+                Console.WriteLine($"El salario de {Nombre} es {SalarioBase + 2000}");
+            }
+            else if (Puesto == "Desarrollador")
+            {
+                Console.WriteLine($"El salario de {Nombre} es {SalarioBase + 1000}");
+            }
+            else if (Puesto == "Becario")
+            {
+                Console.WriteLine($"El salario de {Nombre} es {SalarioBase + 500}");
+            }
+            else
+            {
+                Console.WriteLine($"El salario de {Nombre} es {SalarioBase}");
+            }
+        }
+
+        public void GuardarEnBaseDeDatos()
+        {
+            Console.WriteLine($"Guardando {Nombre} en la base de datos...");
+            // Código para guardar el empleado en la base de datos
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado emp1 = new Empleado("Alicia", "Gerente", 5000);
+            emp1.CalcularSalario();
+            emp1.GuardarEnBaseDeDatos();
+        }
+    }
+}
